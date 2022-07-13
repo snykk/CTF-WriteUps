03bf65ff34bda7efafd5d83aa0186a0bde996ba6
Najib Fikri <najibfikri13@gmail.com>
2022-07-13 12:37:24 +0700

    Add "PWCrack3" sources

---
+++ PicoCTF/General Skills/PW Crack 3/solve.py
@@ -0,0 +1,42 @@
+import hashlib
+
+### THIS FUNCTION WILL NOT HELP YOU FIND THE FLAG --LT ########################
+def str_xor(secret, key):
+    #extend key to secret length
+    new_key = key
+    i = 0
+    while len(new_key) < len(secret):
+        new_key = new_key + key[i]
+        i = (i + 1) % len(key)        
+    return "".join([chr(ord(secret_c) ^ ord(new_key_c)) for (secret_c,new_key_c) in zip(secret,new_key)])
+###############################################################################
+
+flag_enc = open('level3.flag.txt.enc', 'rb').read()
+correct_pw_hash = open('level3.hash.bin', 'rb').read()
+
+
+def hash_pw(pw_str):
+    pw_bytes = bytearray()
+    pw_bytes.extend(pw_str.encode())
+    m = hashlib.md5()
+    m.update(pw_bytes)
+    return m.digest()
+
+
+def level_3_pw_check(pw):
+    user_pw = pw
+    user_pw_hash = hash_pw(user_pw)
+    
+    if( user_pw_hash == correct_pw_hash ):
+        decryption = str_xor(flag_enc.decode(), user_pw)
+        print(decryption)
+        return
+
+
+# The strings below are 7 possibilities for the correct password. 
+#   (Only 1 is correct)
+pos_pw_list = ["f09e", "4dcf", "87ab", "dba8", "752e", "3961", "f159"]
+
+for i in pos_pw_list:
+	level_3_pw_check(i)
+
